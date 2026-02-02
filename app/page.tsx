a1aae011cbc910a841081617633a6519a13f457c
majazocom <maja.gedda@zocom.se>
2026-02-02 10:46:21 +0100

    created pokemon component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 async function getPokemon() {
   try {
     // Försök hämta data från API:et
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20");
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20",
+    );
 
     // Om svaret inte är OK (t.ex 404, 500)
     if (!response.ok) {
@@ -24,5 +26,30 @@ async function getPokemon() {
   }
 }
 
-
 // 2. Komponenten
+export default async function HomePage() {
+  try {
+    // Försök hämta Pokémon
+    const pokemonList = await getPokemon();
+
+    return (
+      <main>
+        <h1>Min Poké-Shop</h1>
+        <p>Denna data hämtas direkt vid sidladdning.</p>
+        <ul>
+          {pokemonList.map((pokemon: { name: string }) => (
+            <li key={pokemon.name}>{pokemon.name}</li>
+          ))}
+        </ul>
+      </main>
+    );
+  } catch (error) {
+    // Om något går fel -> visa fallback-UI istället
+    return (
+      <main>
+        <h1>Något gick fel</h1>
+        <p>Vi kunde tyvärr inte ladda några Pokémon just nu.</p>
+      </main>
+    )
+  }
+}
